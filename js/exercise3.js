dce259f76b4f78d9060991bdc0c42f075034011b
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-16 20:41:50 -0700

    converted the flowchart to JS code for exercise3.js

---
+++ js/exercise3.js
@@ -0,0 +1,14 @@
+//exercise3.js
+let num1 = 8;
+let num2 = 10;
+let num3 = 15;
+
+if(num1 > num2){
+    if(num1 > num3){
+        console.log(num1);
+    }
+}else if(num2 > num3){
+    console.log(num2);
+}else{
+    console.log(num3);
+}
