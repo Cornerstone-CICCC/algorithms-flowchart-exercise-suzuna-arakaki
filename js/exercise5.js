fa032e565b81639ff42a70a3c631f023ec0ce41d
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-16 20:54:56 -0700

    converted the flowchart to JS code for exercise5.js

---
+++ js/exercise5.js
@@ -0,0 +1,13 @@
+//exercise5.js
+let studentName = "Elmer";
+let studentScore = 99;
+
+if (studentScore >= 80){
+    console.log("A");
+}else if (studentScore >= 60){
+    console.log("B");
+}else if (studentScore >= 40){
+    console.log("C");
+}else{
+    console.log("No Grade");
+}
